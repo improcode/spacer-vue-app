b7868241a65734479cfe1aac82e391ef2137ffc5
improcode <art.milkowski@gmail.com>
2019-03-16 17:50:59 +0100

    connect app to NASA API

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="wrapper">
+    <Claim/>
     <div class="search">
-    <label for="search">Search</label>
-      <input
-        id="search"
-        name="search"
-        v-model="searchValue"
-        @input="handleInput "
-      />
+      <label for="search">Search</label>
+      <input id="search" name="search" v-model="searchValue" @input="handleInput">
       <ul>
         <li v-for="item in results" :key="item.data[0].nasa_id">
-          <p>{{ item.data[0].description }}</p>
+          <p>{{ item.data[0].description}}</p>
         </li>
       </ul>
     </div>
@@ -18,61 +14,63 @@
 </template>
 
 <script>
-import axios from 'axios';
-import debounce from 'lodash.debounce';
+import axios from "axios";
+import debounce from "lodash.debounce";
+// eslint-disable-next-line
+import Claim from "../components/Claim";
 
-const API = 'https://images-api.nasa.gov/search';
+const API = "https://images-api.nasa.gov/search";
 
 export default {
-  name: 'Search',
+  name: "Search",
+  components: { Claim },
   data() {
     // funkcja, która zwraca obiekt
     return {
-      searchVaule: '',
+      searchVaule: "",
       // musi zostać nadana inicjalna wartość, inaczej nie będzie ona reaktywna
-      results: [],
+      results: []
     };
   },
   methods: {
     // eslint-disable-next-line
-    handleInput: debounce(function () {
-      axios.get(`${API}?q=${this.searchValue}&media_type=image`)
-        .then((response) => {
+    handleInput: debounce(function() {
+      axios
+        .get(`${API}?q=${this.searchValue}&media_type=image`)
+        .then(response => {
           this.results = response.data.collection.items;
         })
-        .catch((error) => {
+        .catch(error => {
           console.log(error);
         });
-    }, 500),
-  },
-
+    }, 500)
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-  .wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 0;
-    padding: 30px;
-    width: 100%;
-  }
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 30px;
+  width: 100%;
+}
 
-  .search {
-    display: flex;
-    flex-direction: column;
-    width: 250px;
-  }
+.search {
+  // display: none;
+  flex-direction: column;
+  width: 250px;
+}
 
-  label {
-    font-family: Montserrat, sans-serif;
-  }
+label {
+  font-family: Montserrat, sans-serif;
+}
 
-  input {
-    height: 30px;
-    border: 0;
-    border-bottom: 1px solid black;
-  }
-
+input {
+  height: 30px;
+  border: 0;
+  border-bottom: 1px solid black;
+}
 </style>
